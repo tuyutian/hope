17f330e45269378bb80f7f49c9cc0db757684d7a
tuyutian <1635358408@qq.com>
2025-08-02 12:55:46 +0800

    chore: remove sensitive and unused environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   base: "/",
   plugins: [tailwindcss(), react(), tsconfigPaths()],
   build: {
+    manifest: true,
     sourcemap: false,
     rollupOptions: {
       output: {
